2b66b2815e9be88f6619d6e3bc15dd3ae8838685
Jitesh Saini <148760926+jitesh-saini-dev@users.noreply.github.com>
2026-04-25 00:06:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (30.54%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int left = 0;
-        int right = 0;
+        int pos = 0;
         int blank = 0;
 
-        for (auto x : moves) {
-            if (x == 'L') left++;
-            else if (x == 'R') right++;
+        for (char ch : moves) {
+            if (ch == 'L') pos--;
+            else if (ch == 'R') pos++;
             else blank++;
         }
 
-        int condition1 = abs(right - left + blank);
-        int condition2 = abs(left - right + blank);
-
-        return max(condition1, condition2);
+        return abs(pos) + blank;
     }
 };
