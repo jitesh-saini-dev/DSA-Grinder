2ad8cbcde660f12fad458c29f8e5978168799f6d
Jitesh Saini <148760926+jitesh-saini-dev@users.noreply.github.com>
2026-07-21 23:24:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (61.99%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n = s.size();
+        int low = 0, high = n;
+        vector<int> ans;
+
+        for (char ch : s) {
+            if (ch == 'I') {
+                ans.push_back(low++);
+            } else {
+                ans.push_back(high--);
+            }
+        }
+
+        ans.push_back(low); // low == high
+        return ans;
+    }
+};
