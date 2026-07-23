1e169911d1910b85dc5d67e28ab5b997b5b1db51
Jitesh Saini <148760926+jitesh-saini-dev@users.noreply.github.com>
2026-07-24 00:00:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.14%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int i = 0;
+
+        // 1. Skip leading whitespaces
+        while (i < n && s[i] == ' ')
+            i++;
+
+        // 2. Check sign
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
+            i++;
+        }
+
+        // 3. Convert digits
+        long long num = 0;
+
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            // 4. Overflow check
+            if (num > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            num = num * 10 + digit;
+            i++;
+        }
+
+        return sign * num;
+    }
+};
